218d6fb2171c2aa91bbccdeb057fff0a47b8cf08
Sidnei Silva <tjsidnei@gmail.com>
2025-08-16 07:48:57 +0000

    people controller select keys adjustment

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -2,7 +2,16 @@ const pool = require('../db');
 
 async function getPeopleList(req, res) {
   try {
-    const result = await pool.query('SELECT * FROM "territories-people"');
+    const result = await pool.query(
+      `
+        SELECT
+          p.id AS peopleid,
+          p."first-name" AS firstname,
+          p."last-name" AS lastname
+        FROM "territories-people" p
+        ORDER BY firstname ASC;
+      `
+    );
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
